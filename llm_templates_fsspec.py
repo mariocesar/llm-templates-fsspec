4d0fb9dc9d86a75ac456dba827e1f7c7dcf21f8c
Mario-César <mariocesar.c50@gmail.com>
2025-06-06 16:51:31 -0400

    Register loader

--- llm_templates_fsspec.py
+++ llm_templates_fsspec.py
@@ -3,7 +3,7 @@ from llm import Template, hookimpl
 
 @hookimpl
 def register_template_loaders(register):
-    register("local", fsspec_template_loader)
+    register("fsspec", fsspec_template_loader)
 
 
 def fsspec_template_loader(template_path: str) -> Template:
